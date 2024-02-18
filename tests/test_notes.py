ad866b9c8228e98021de052de969578f4517aa02
Joe Plattenburg <joeplattenburg@gmail.com>
2024-02-18 17:57:59 -0500

    refactor and add Guitar class to support other tunings

--- tests/test_notes.py
+++ tests/test_notes.py
@@ -1,3 +1,5 @@
+import pytest
+
 import notes
 
 
@@ -11,3 +13,34 @@ def test_positions_found_with_lower_notes_on_higher_strings() -> None:
     ]
     actual = chord.guitar_positions()
     assert actual == expected
+
+
+@pytest.mark.parametrize(
+    'frets,expected',
+    [
+        (10, {'E': 8, 'A': 3}),
+        (5, {'A': 3})
+    ]
+)
+def test_guitar_with_different_fret_count(frets: int, expected: dict[str, int]) -> None:
+    guitar = notes.Guitar(frets=frets)
+    note = notes.Note('C', 3)
+    actual = note.guitar_positions(guitar)
+    assert actual == expected
+
+@pytest.mark.parametrize(
+    'strings',
+    [
+        [('E', 2), ('A', 2), ('D', 3), ('G', 3), ('B', 3), ('E', 4)],
+        [('D', 2), ('A', 2), ('D', 3), ('G', 3), ('A', 3), ('D', 4)],
+        [('B', 1), ('E', 2), ('A', 2), ('D', 3), ('G', 3), ('B', 3), ('E', 4), ('A', 4)],
+    ]
+)
+def test_different_guitar_tunings(strings: list[tuple[str, int]]) -> None:
+    guitar = notes.Guitar(
+        tuning={i: notes.Note(*string) for i, string in enumerate(strings)}
+    )
+    chord = notes.Chord([notes.Note(*string) for string in strings])
+    expected = [{i: 0 for i in range(len(strings))}]
+    acutal = chord.guitar_positions(guitar=guitar)
+    assert acutal == expected
