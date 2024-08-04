cee0b675322c43a155ba1e0e4ce87aaaacbb02ab
Joe Plattenburg <joeplattenburg@gmail.com>
2024-08-04 09:44:33 -0400

    update app to accept post requests with user input and respond

--- app.py
+++ app.py
@@ -1,24 +1,35 @@
-import notes
+import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 from markupsafe import escape
 
+import notes
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24).hex()
 
 
-@app.route("/demo")
-def info():
-    return render_template('info.html')
+@app.route("/", methods=('GET', 'POST'))
+def input():
+    if request.method == 'POST':
+        notes_string = request.form['notes']
+        top_n = request.form['top_n']
+        if not notes_string:
+            flash('Notes are required!')
+        elif not top_n:
+            flash('Number of positions is required!')
+        else:
+            return redirect(url_for('display', notes_string=notes_string, top_n=top_n))
+    return render_template('input.html')
 
 
-@app.route("/demo/<notes_string>/<int:top_n>/")
-def demo(notes_string: str, top_n: int) -> str:
+@app.route("/<notes_string>/<int:top_n>/")
+def display(notes_string: str, top_n: int) -> str:
     notes_list = [notes.Note.from_string(note) for note in escape(notes_string).split(',')]
     chord = notes.Chord(notes_list)
     positions = chord.guitar_positions()[:top_n]
     positions_printable = ['<br>'.join(p.printable()) for p in positions]
     return render_template(
-        'demo.html',
+        'display.html',
         chord=chord, top_n=top_n, positions=positions_printable
     )
