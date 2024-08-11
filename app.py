82ac0811a5a5b00d9931c3e0f1b797c377d0c3e6
Joe Plattenburg <joeplattenburg@gmail.com>
2024-08-11 19:33:19 -0400

    update app

--- app.py
+++ app.py
@@ -18,30 +18,48 @@ def input():
         top_n = request.form['top_n'] or 5
         notes_string = request.form['notes']
         chord_name = request.form['chord_name']
-        if not notes_string:
-            if not chord_name:
-                flash('Either notes or name are required!')
-            else:
-                chord = notes.ChordName(chord_name).get_chord(lower=guitar.lowest)
-                notes_string = str(chord)
-        return redirect(url_for('display', notes_string=notes_string, top_n=top_n, tuning=tuning))
+        if notes_string:
+            return redirect(url_for('display_notes', notes_string=notes_string, top_n=top_n, tuning=tuning))
+        elif chord_name:
+            return redirect(url_for('display_name', chord_name=chord_name, top_n=top_n, tuning=tuning))
+        else:
+            flash('Either notes or name are required!')
     return render_template('input.html')
 
 
-@app.route("/<notes_string>/<int:top_n>/<tuning>/")
-def display(notes_string: str, top_n: int, tuning: str) -> str:
+@app.route("/notes/<notes_string>/<int:top_n>/<tuning>/")
+def display_notes(notes_string: str, top_n: int, tuning: str) -> str:
     notes_list = [notes.Note.from_string(note) for note in escape(notes_string).split(',')]
     chord = notes.Chord(notes_list)
-    print('tuning')
     guitar = (
         notes.Guitar() if tuning == 'standard' else
         notes.Guitar(tuning=notes.Guitar.parse_tuning(tuning.split(';')[1]))
     )
-    positions = chord.guitar_positions(guitar=guitar)[:top_n]
+    positions_all = chord.guitar_positions(guitar=guitar)
+    positions = positions_all[:top_n]
     positions_printable = ['<br>'.join(p.printable()) for p in positions]
     return render_template(
         'display.html',
-        chord=chord, tuning=tuning, top_n=top_n, positions=positions_printable
+        chord=chord, tuning=tuning, top_n=top_n, positions=positions_printable, total_n=len(positions_all)
+    )
+
+
+@app.route("/chord_name/<chord_name>/<int:top_n>/<tuning>/")
+def display_name(chord_name: str, top_n: int, tuning: str) -> str:
+    guitar = (
+        notes.Guitar() if tuning == 'standard' else
+        notes.Guitar(tuning=notes.Guitar.parse_tuning(tuning.split(';')[1]))
+    )
+    chord_name = chord_name.replace('_', '/')
+    chords = notes.ChordName(chord_name).get_all_chords(lower=guitar.lowest, upper=guitar.highest)
+    positions_all = []
+    for chord in chords:
+        positions_all += chord.guitar_positions(guitar=guitar)
+    positions = sorted(positions_all, key=lambda x: x.fret_span)[:top_n]
+    positions_printable = ['<br>'.join(p.printable()) for p in positions]
+    return render_template(
+        'display.html',
+        chord=chord_name, tuning=tuning, top_n=top_n, positions=positions_printable, total_n=len(positions_all)
     )
 
 
