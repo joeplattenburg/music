87236e128bbb4a2d53456acbc85337fe55a453d3
Joe Plattenburg <joeplattenburg@gmail.com>
2024-08-30 21:56:48 -0400

    extend duration

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def display_notes(notes_string: str, top_n: str, max_fret_span: str, tuning: str
     allow_thumb_: bool = escape(allow_thumb).split('=')[1] == 'true'
     notes_list = [music.Note.from_string(note) for note in escape(notes_string).split(',')]
     chord = music.Chord(notes_list)
-    chord.write_wav(os.path.join(PROJ_DIR, 'static', 'temp.wav'), duration=2)
+    chord.write_wav(os.path.join(PROJ_DIR, 'static', 'temp.wav'), duration=3)
     guitar = (
         music.Guitar() if tuning_ == 'standard' else
         music.Guitar(tuning=music.Guitar.parse_tuning(tuning_.split(';')[1]))
@@ -104,7 +104,7 @@ def display_name(
     chord = music.ChordName(chord_name_)
     (
         chord.get_chord(lower=music.Note('C', 3))
-        .write_wav(os.path.join(PROJ_DIR, 'static', 'temp.wav'), duration=2)
+        .write_wav(os.path.join(PROJ_DIR, 'static', 'temp.wav'), duration=3)
     )
     positions_all = music.get_all_guitar_positions_for_chord_name(
         chord_name=chord,
